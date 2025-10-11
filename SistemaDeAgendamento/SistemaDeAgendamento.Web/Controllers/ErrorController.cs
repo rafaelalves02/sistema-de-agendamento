01a0c4d59fb3ef96c5012d88692064f635e8950f
Rafael Alves <rafaelalvesdecarvalho25@gmail.com>
2025-10-11 19:07:18 -0300

    agora sim os projetos

---
+++ SistemaDeAgendamento/SistemaDeAgendamento.Web/Controllers/ErrorController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SistemaDeAgendamento.Web.Models.Error;
+using System.Diagnostics;
+
+namespace SistemaDeAgendamento.Web.Controllers
+{
+    public class ErrorController : Controller
+    {
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Index()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ SistemaDeAgendamento/SistemaDeAgendamento.Web/Models/Error/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace SistemaDeAgendamento.Web.Models.Error
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
